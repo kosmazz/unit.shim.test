0eb17b83c7ebced622aac82505a99b59eff3e10a
yuryk <yuryk@YURYK-PC>
2017-01-31 18:38:46 +0300

    Add project files.

---
+++ UnitTestProject1/ShimTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.QualityTools.Testing.Fakes;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using shim.test;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class ShimTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            
+
+            using (ShimsContext.Create())
+            {
+                var a = new TargetClass();
+
+                if (a.DoSomething().Year != 2017)
+                {
+                    Assert.Fail();
+                }
+            }
+        }
+    }
+}
---
+++ shim.test/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace shim.test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+
+    public class TargetClass
+    {
+        public DateTime DoSomething()
+        {
+            return DateTime.Now;
+        }
+    }
+}
